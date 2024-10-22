4c19a5f518f0b6a93fde5c9a5635ab5505fc889d
priyachithiravel50 <priyachithiravel50@gmail.com>
2024-10-22 16:04:07 +0530

    Today Updates

--- script.js
+++ script.js
@@ -44,17 +44,26 @@
 
   
 
-const form = document.querySelector('#form');
-const username = document.querySelector('#username');
-const email = document.querySelector('#email');
-const password = document.querySelector('#password');
-const cpassword = document.querySelector('#cpassword');
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const cpassword = document.getElementById('cpassword');
 
 form.addEventListener('submit',(e)=>{
     
     if(!validateInputs()){
         e.preventDefault();
     }
+
+    // const users=[];
+    //     const newUser={userName,email,password,cpassword};
+    //     users.push(newUser);
+    //     console.log(users)
+    //     document.getElementById('userName').value=""
+    //     document.getElementById('email').value=""
+    //     document.getElementById('password').value=""
+    //     document.getElementById('cpassword').value=""
 })
 
 function validateInputs(){
@@ -136,8 +145,7 @@ const validateEmail = (email) => {
       .match(
          /^[^\s@]+@[^\s@]+\.[^\s@]+$/
 
-    //     /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-      );
+    );
   };
 
 
